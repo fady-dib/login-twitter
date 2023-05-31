b77e7e64cd7b1b8f52e02dbfdb3e91611bc577c0
Fady Dib <fady.dib@hotmail.com>
2023-06-01 01:59:41 +0300

    Add auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+const User = require('../models/userModel');
+
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: "http://localhost:3005/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, cb) {
+    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+module.exports = passport;
