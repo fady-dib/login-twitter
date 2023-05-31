6186a456edf41ace58c9a51929442b72a0f68b26
Fady Dib <fady.dib@hotmail.com>
2023-06-01 01:50:51 +0300

    Add user model

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.listen(process.env.PORT, (err) => {
     if (err) console.log (err)
     console.log("Server is running on port ", process.env.PORT||3007);
